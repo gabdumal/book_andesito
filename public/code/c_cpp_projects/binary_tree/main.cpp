47bb5ed6d2d64929735a73c3bd0573dbc8ed799f
Gabriel Malosto <professional@gabdumal.com>
2024-11-30 07:21:06 -0300

    Fix links for public files

---
+++ public/code/c_cpp_projects/binary_tree/main.cpp
@@ -0,0 +1,22 @@
+#include "tree/tree.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  Tree tree;
+
+  tree.insert(5);
+  tree.insert(3);
+  tree.insert(8);
+  tree.insert(1);
+  tree.insert(4);
+  tree.insert(7);
+  tree.insert(9);
+
+  tree.print();
+
+  cout << endl;
+
+  return 0;
+}
